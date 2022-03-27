5d671ba82f5bbecc805e7e47e761117e3e171c99
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-27 23:32:42 +0100

    09_f

---
+++ 09_codigo_morse/solucion/js/index.js
@@ -0,0 +1,12 @@
+
+// Implementar un traductor de texto a código morse.
+
+const input = document.getElementById('input');
+const resultados = document.getElementById('resultados');
+
+input.addEventListener('input', ()=>{
+    resultados.innerText = '';
+    const valor = input.value.toLowerCase().split('');
+    const valorTraducido = valor.map(letra=> `${CODIGO_MORSE[letra]}` ?? letra).join('');
+    resultados.innerText = valorTraducido;
+})
