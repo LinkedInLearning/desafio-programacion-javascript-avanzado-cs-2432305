7a9bbc77d3e432ee67f17afd08371be65e4f9006
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-27 23:37:56 +0100

    14_i

---
+++ 14_aplanar_arreglos/ejercicio/js/index.js
@@ -0,0 +1,8 @@
+// Implementar solución para aplanar el arreglo:
+
+const arreglo = [
+    [1, 2],
+    [3, [7, 8, [90, 2, 13]]],
+    [5, [7, 8, 9]],
+    [10, 11, 12, [7, 8, [90, 2, 13]], 14, 15]
+];
