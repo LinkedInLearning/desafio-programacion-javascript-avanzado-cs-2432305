bf2d0f9b81b1b3e62cfae2dd5a4b0fb3723b8f35
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-27 23:22:24 +0100

    02_i

---
+++ 02_objetos_iguales/ejercicio/js/index.js
@@ -0,0 +1,13 @@
+
+// Escribir una función que determine si dos objetos son iguales:
+
+function sonIguales(obj1, obj2) {
+    
+}
+// Todos los console logs deben retornar true:
+console.log(sonIguales(2, 2));
+console.log(sonIguales([1, 2, 3], [1, 2, 3]));
+console.log(sonIguales({ a: 1, b: 2 }, { a: 1, b: 2 }));
+console.log(sonIguales([{ a: 1, b: 2 }], [{ a: 1, b: 2 }]));
+console.log(sonIguales([{ a: [100, 200], b: 2 }], [{ a: [100, 200], b: 2 }]));
+console.log(sonIguales([{ a: [100, 200], b: { x: 'ABC' } }], [{ a: [100, 200], b: { x: 'ABC' } }]));
