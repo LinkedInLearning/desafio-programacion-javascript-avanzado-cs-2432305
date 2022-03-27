216c6c5f3dcee0c16aa1baf279f7233f39bf267f
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-27 23:25:58 +0100

    05_i

---
+++ 05_coleccion_artistica/ejercicio/js/index.js
@@ -0,0 +1,6 @@
+
+// Cargar las imágenes contenidas en la constante rutasImagenes utilizando Promise.all
+// Luego cargadas, agregarlas al div #contenedor
+
+const rutasImagenes = imagenes;
+const contenedor = document.getElementById('contenedor');
