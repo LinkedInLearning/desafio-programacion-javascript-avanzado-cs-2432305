65378198862ee65bc51be51ddd4f2c818d5e5971
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-27 23:31:33 +0100

    08_i

---
+++ 08_que_hora_es_alla/ejercicio/js/index.js
@@ -0,0 +1,52 @@
+
+const zonasHorarias = [
+    {
+        "offset": -10,
+        "texto": "(UTC-10:00) Hawaii",
+    },
+    {
+        "offset": -6,
+        "texto": "(UTC-06:00) Central America",
+    },
+    {
+        "offset": -8,
+        "texto": "(UTC-09:00) Alaska",
+    },
+    {
+        "offset": -7,
+        "texto": "(UTC-07:00) Pacific Daylight Time (US & Canada)",
+    },
+    {
+        "offset": -7,
+        "texto": "(UTC-07:00) Arizona",
+    },
+    {
+        "offset": 1,
+        "texto": "(UTC) Dublin, Lisbon",
+    },
+    {
+        "offset": 0,
+        "texto": "(UTC) Greenwich Standard Time",
+    },
+    {
+        "offset": 2,
+        "texto": "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna",
+    },
+
+    {
+        "offset": 4,
+        "texto": "(UTC+04:00) Abu Dhabi, Muscat",
+    },
+    {
+        "offset": 5.5,
+        "texto": "(UTC+05:30) Chennai, Kolkata, Mumbai, New Delhi",
+    },
+    {
+        "offset": 7,
+        "texto": "(UTC+07:00) Bangkok, Hanoi, Jakarta",
+    },
+    {
+        "offset": 8,
+        "texto": "(UTC+08:00) Perth",
+    }
+];
